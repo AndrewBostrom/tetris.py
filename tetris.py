b5b9d61c43d17eeb7dbb54a9c4b608926bded2f6
Andrew <bostr049@umn.edu>
2016-10-16 23:41:29 -0500

    Wrote some of rotateBlock function

--- tetris.py
+++ tetris.py
@@ -43,6 +43,7 @@ class TetrisGUI():
                 self.activeBlockList = []
                 self.score = 0
                 self.pause = False
+                self.blockRotation= 0
 
                 self.createBlock()
                 self.window.bind('<KeyPress>', self.keyPressed)
@@ -63,66 +64,154 @@ class TetrisGUI():
                 self.color = colorList[blockList.index(self.block)]
 
                 if self.block == "t":
-                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color)
-                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color)
-                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color)
-                        self.page.create_rectangle(100, 40, 120, 60, fill=self.color)
+                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 40, 120, 60, fill=self.color, tag="active")
                         activeBlockList = [(80, 20), (100, 20), (120, 20), (100, 40)]
 
                 elif self.block == "s":
-                        self.page.create_rectangle(80, 40, 100, 60, fill=self.color)
-                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color)
-                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color)
-                        self.page.create_rectangle(100, 40, 120, 60, fill=self.color)
+                        self.page.create_rectangle(80, 40, 100, 60, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 40, 120, 60, fill=self.color, tag="active")
                         activeBlockList = [(80, 40), (100, 20), (120, 20), (100, 40)]
                         
                 elif self.block == "z":
-                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color)
-                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color)
-                        self.page.create_rectangle(100, 40, 120, 60, fill=self.color)
-                        self.page.create_rectangle(120, 40, 140, 60, fill=self.color)
+                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 40, 120, 60, fill=self.color, tag="active")
+                        self.page.create_rectangle(120, 40, 140, 60, fill=self.color, tag="active")
                         activeBlockList = [(80, 20), (100, 20), (100, 40), (120, 40)]
                         
                 elif self.block == "j":
-                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color)
-                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color)
-                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color)
-                        self.page.create_rectangle(120, 40, 140, 60, fill=self.color)
+                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(120, 40, 140, 60, fill=self.color, tag="active")
                         activeBlockList = [(80, 20), (100, 20), (120, 20), (120, 40)]
                         
                 elif self.block == "l":
-                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color)
-                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color)
-                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color)
-                        self.page.create_rectangle(80, 40, 100, 60, fill=self.color)
+                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(80, 40, 100, 60, fill=self.color, tag="active")
                         activeBlockList = [(80, 20), (100, 20), (120, 20), (80, 40)]
                         
                 elif self.block == "i":
-                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color)
-                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color)
-                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color)
-                        self.page.create_rectangle(140, 20, 160, 40, fill=self.color)
+                        self.page.create_rectangle(80, 20, 100, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(140, 20, 160, 40, fill=self.color, tag="active")
                         activeBlockList = [(80, 20), (100, 20), (120, 20), (140, 20)]
                         
                 elif self.block == "o":
-                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color)
-                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color)
-                        self.page.create_rectangle(100, 40, 120, 60, fill=self.color)
-                        self.page.create_rectangle(120, 40, 140, 60, fill=self.color)
+                        self.page.create_rectangle(100, 20, 120, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(120, 20, 140, 40, fill=self.color, tag="active")
+                        self.page.create_rectangle(100, 40, 120, 60, fill=self.color, tag="active")
+                        self.page.create_rectangle(120, 40, 140, 60, fill=self.color, tag="active")
                         activeBlockList = [(100, 20), (120, 20), (100, 40), (120, 40)]
                         
                 for activeBlockCoord in self.activeBlockList:
                         if self.coordList.contains(self.activeBlockCoord):
-                                gameOver()
+                                self.gameOver()
                 
         def keyPressed(self, event):
-                print()
+                if event.keysym == "Up":
+                        self.rotateBlock()
+                elif event.keysym == "Down":
+                        print()
+                elif event.keysym == "Left":
+                        self.moveBlockLeft()
+                elif event.keysym == "Right":
+                        self.moveBlockRight()
+                
         def moveBlock(self):
                 print()
         def moveBlockLeft(self):
                 print()
         def moveBlockRight(self):
                 print()
+        def rotateBlock(self):
+                if self.block == "o":
+                        return
+                if not self.canRotate():
+                        return
+                blockList = ("t", "s", "z", "j", "l", "i", "o")
+                if self.block == "t":
+                        if self.blockRotation == 0:
+
+                                self.blockRotation = 1
+                        elif self.blockRotation == 1:
+
+                                self.blockRotation = 2
+                        elif self.blockRotation == 2:
+
+                                self.blockRotation = 3
+                        else:
+
+                                self.blockRotation = 0
+                elif self.block == "s":
+                        if self.blockRotation == 0:
+
+                                self.blockRotation = 1
+                        else:
+
+                                self.blockRotation = 0
+                elif self.block == "z":
+                        if self.blockRotation == 0:
+
+                                self.blockRotation = 1
+                        else:
+
+                                self.blockRotation = 0
+                elif self.block == "j":
+                        if self.blockRotation == 0:
+
+                                self.blockRotation = 1
+                        elif self.blockRotation == 1:
+
+                                self.blockRotation = 2
+                        elif self.blockRotation == 2:
+
+                                self.blockRotation = 3
+                        else:
+
+                                self.blockRotation = 0
+                elif self.block == "l":
+                        if self.blockRotation == 0:
+                                
+                                self.blockRotation = 1
+                        elif self.blockRotation == 1:
+
+                                self.blockRotation = 2
+                        elif self.blockRotation == 2:
+
+                                self.blockRotation = 3
+                        else:
+
+                                self.blockRotation = 0
+
+                elif self.block == "i":
+                        if self.blockRotation == 0:
+                                self.activeBlockList[0] = (self.activeBlockList[2][0]-40, self.activeBlockList[2][1])
+                                self.activeBlockList[1] = (self.activeBlockList[2][0]-20, self.activeBlockList[2][1])
+                                self.activeBlockList[3] = (self.activeBlockList[2][0]+20, self.activeBlockList[2][1])
+
+                                self.page.delete("active")
+                                self.page.create_rectangle(self.activeBlockList[0][0], self.activeBlockList[0][1], self.activeBlockList[0][0]+20, self.activeBlockList[0][1]+20, fill=self.color)
+                                self.page.create_rectangle(self.activeBlockList[1][0], self.activeBlockList[1][1], self.activeBlockList[1][0]+20, self.activeBlockList[1][1]+20, fill=self.color)
+                                self.page.create_rectangle(self.activeBlockList[2][0], self.activeBlockList[2][1], self.activeBlockList[2][0]+20, self.activeBlockList[2][1]+20, fill=self.color)
+                                self.page.create_rectangle(self.activeBlockList[3][0], self.activeBlockList[3][1], self.activeBlockList[3][0]+20, self.activeBlockList[3][1]+20, fill=self.color)
+
+                                self.blockRotation = 1
+                        else:
+
+                                self.blockRotation = 0
+
+        def canRotate(self):
+                #check if rotating will put block inside another
+                return True
         def gameOver(self):
                 self.pause = True
                 self.page.delete("activeBlock", "inactiveBlock")
